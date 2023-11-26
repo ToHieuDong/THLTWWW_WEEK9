f13fa4bc1d8da8179f951a9686c1846b9823da64
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2023-11-26 09:22:02 +0700

    init commit

---
+++ src/main/java/com/example/thwww_week9/ThwwwWeek9Application.java
@@ -0,0 +1,13 @@
+package com.example.thwww_week9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThwwwWeek9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThwwwWeek9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thwww_week9/ThwwwWeek9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thwww_week9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThwwwWeek9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
