60b5b61005a7551ecbb00cbd3df507adeba51efa
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2023-11-26 10:23:42 +0700

    mem and h2 config security

---
+++ src/main/java/com/example/thwww_week9/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.example.thwww_week9.config;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+//    @Autowired
+//    public void configSecurityWeb (AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder ) throws Exception {
+//        auth.inMemoryAuthentication()
+//            .withUser(User.withUsername("Gnoodd")
+//                .password(passwordEncoder.encode("Gnoodd"))
+//                .roles("ADMIN")
+//                .build())
+//        .withUser(User.withUsername("Dong")
+//                .password(passwordEncoder.encode("Dong"))
+//                .roles("User")
+//                .build());
+//
+//    }
+    @Autowired
+    public void configSecurityWeb (AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder, DataSource dataSource) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+            .withUser(User.withUsername("Gnoodd")
+                .password(passwordEncoder.encode("Gnoodd"))
+                .roles("ADMIN")
+                .build())
+        .withUser(User.withUsername("Dong")
+                .password(passwordEncoder.encode("Dong"))
+                .roles("User")
+                .build());
+
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/home", "/index").hasRole("ADMIN")
+                .requestMatchers("/api/**").hasRole("USER")
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+
+        );
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
---
+++ src/main/java/com/example/thwww_week9/controllers/AdminController.java
@@ -0,0 +1,15 @@
+package com.example.thwww_week9.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "admin";
+    }
+}
